8b752bac5c3c66a9e2e5cc4e85d525d54b0bdcbf
zaccrabtree <zac.crabtree1@gmail.com>
2023-01-25 21:41:08 -0600

    modified printJobs to iterate through Arraylist of Hashmaps and print out key-value pairs per specs of task number 1, but first test being difficult to please.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> jobEntry : job.entrySet()) {
+                    System.out.println(jobEntry.getKey() + ": " + jobEntry.getValue());
+                }
+                System.out.println("*****\n");
+            }
+        }
+        }
     }
-}
+
